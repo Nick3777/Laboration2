2256270419ae2752f17a6a00a13012fab172137b
Nick3777 <brandonicco@gmail.com>
2023-03-20 18:47:58 +0100

    Added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule} from '@angular/fire/compat/firestore';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyALt3Zvd--jvkGT13Y1MkOyye5h_yFrRC8",
+        authDomain: "ik163-lab2.firebaseapp.com",
+        projectId: "ik163-lab2",
+        storageBucket: "ik163-lab2.appspot.com",
+        messagingSenderId: "745545567638",
+        appId: "1:745545567638:web:b4179ee4c96c471ed521b7",
+        measurementId: "G-CB0M2Q2HTJ"
+    }
+};
