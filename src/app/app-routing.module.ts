cfbb57c3a11ac2aad88d41daab19b34b88aae1b4
Nick3777 <brandonicco@gmail.com>
2023-03-22 15:26:39 +0100

    Added the layout framework and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InputComponent } from './co/input/input.component';
+import { DefaultComponent } from "./co/default/default.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: InputComponent },
+  { path: '', component: DefaultComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,15 @@ import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { environment } from '../environments/environment';
 import { InputComponent } from './co/input/input.component';
+import { MenuComponent } from './co/menu/menu.component';
+import { DefaultComponent } from './co/default/default.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InputComponent,
+    MenuComponent,
+    DefaultComponent,
   ],
   imports: [
     BrowserModule,
