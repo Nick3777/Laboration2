da97a7168582086407c9de62b4c4391e566d21a6
hannes3000 <moritz.hansen@mailbox.org>
2023-04-04 21:07:25 +0200

    Added sleek new navbar from https://material.angular.io/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,20 +16,29 @@ import { IconicSongComponent } from './co/iconic-song/iconic-song.component';
 import { AddSongComponent } from './co/add-song/add-song.component';
 import { LoggedComponent } from './co/logged/logged.component';
 import { SongComponent } from './co/song/song.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './co/navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    InputComponent,
-    MenuComponent,
-    DefaultComponent,
-    BestSongComponent,
-    IconicSongComponent,
-    AddSongComponent,
-    LoggedComponent,
-    SongComponent
-  ],
+    declarations: [
+        AppComponent,
+        InputComponent,
+        MenuComponent,
+        DefaultComponent,
+        BestSongComponent,
+        IconicSongComponent,
+        AddSongComponent,
+        LoggedComponent,
+        SongComponent,
+        NavigationComponent
+    ],
     imports: [
         BrowserModule,
         AppRoutingModule,
@@ -37,7 +46,14 @@ import { SongComponent } from './co/song/song.component';
         AngularFireModule.initializeApp(environment.firebaseConfig),
         AngularFireDatabaseModule,
         AngularFireAuthModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        LayoutModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatListModule
     ],
   providers: [],
   bootstrap: [AppComponent]
