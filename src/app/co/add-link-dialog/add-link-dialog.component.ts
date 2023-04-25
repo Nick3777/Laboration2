7b1afd63b4fc9ff77ffd56c9eefad9228e9544ef
Nick3777 <brandonicco@gmail.com>
2023-04-25 17:30:44 +0200

    Added logged check to add a song inside the list

--- src/app/co/add-link-dialog/add-link-dialog.component.ts
+++ src/app/co/add-link-dialog/add-link-dialog.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {FormBuilder} from "@angular/forms";
 import {Link} from "../link";
+import {loggedIn} from "../../services/loggedIn";
 import {ServiceService} from "../../services/service.service";
 import { Inject } from '@angular/core';
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-add-link-dialog',
@@ -13,7 +15,8 @@ import { Inject } from '@angular/core';
 export class AddLinkDialogComponent {
   lel = new Link('','','');
   id?: string;
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any,private formBuilder: FormBuilder, private ser: ServiceService, public MatDialogRef: MatDialogRef<AddLinkDialogComponent>) {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any,private formBuilder: FormBuilder, private ser: ServiceService,
+              public MatDialogRef: MatDialogRef<AddLinkDialogComponent>, private logged: loggedIn, private snacki: MatSnackBar) {
     this.lel = new Link(this.data.title, this.data.desc, this.data.url);
     this.id=this.data.id;
   }
@@ -26,20 +29,22 @@ export class AddLinkDialogComponent {
     console.log(this.lel.title);
   }
   save(){
-
-    if(this.id &&this.id !=''){
-      console.log('Houston, were updating!');
+  if(this.logged.log) {
+    if (this.id && this.id != '') {
+      console.log('Houston, we are updating!');
       this.ser.update(this.id, {
         title: <string>this.form.get('title')?.value,
         url: <string>this.form.get('url')?.value,
         desc: <string>this.form.get('desc')?.value
       });
-    }
-    else{
+    } else {
       this.lel = new Link(<string>this.form.get('title')?.value, <string>this.form.get('desc')?.value, <string>this.form.get('url')?.value);
       this.ser.create(this.lel);
     }
     this.MatDialogRef.close();
+  } else{
+    this.snacki.open('You are not logged in', 'Close', {duration: 3000});
+  }
   }
   close(){
     this.MatDialogRef.close();
