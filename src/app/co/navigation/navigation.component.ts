bcc8f3b8a6268d0c69fe826ec1276e8e9a9e52a8
Nick3777 <brandonicco@gmail.com>
2023-04-25 15:42:48 +0200

    Adjustments, styled loggedIn component and added the logged user to  the toolbar

--- src/app/co/navigation/navigation.component.ts
+++ src/app/co/navigation/navigation.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import {loggedIn} from "../../services/loggedIn";
 
 @Component({
   selector: 'app-navigation',
@@ -16,6 +17,24 @@ export class NavigationComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, private service: loggedIn) {}
+
+  logDisplay(){
+    if (this.service.log)
+    {return 'Logged'}
+    return 'Login'
+  }
+  logRouter(){
+    if (this.service.log)
+    {return 'logged'}
+    return 'login'
+  }
+
+  loggedUser(){
+    if(this.service.log)
+      return `Logged in as: ${this.service.nickn}`;
+    else
+      return "Not logged in";
+  }
 
 }
